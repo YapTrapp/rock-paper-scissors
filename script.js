46ae89a5dc75258169be709905651b7d17604d4e
YapTrap <280863229+YapTrapp@users.noreply.github.com>
2026-07-15 20:20:15 -0500

    Add win conditions and event listeners

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 
 
     // Declare game elements 
-    const choices = ["rock", "paper", "scissors"];
+    const rock = document.getElementById("rock");
+    rock.addEventListener("click", () => playGame("rock"));
+    const paper = document.getElementById("paper");
+    paper.addEventListener("click", () => playGame("paper"));
+    const scissors = document.getElementById("scissors");
+    scissors.addEventListener("click", () => playGame("scissors"));
     const playerDisplay = document.getElementById("playerDisplay");
     const computerDisplay = document.getElementById("computerDisplay");
     const resultDisplay = document.getElementById("resultDisplay");
@@ -10,15 +15,19 @@
     const computerScoreDisplay = document.getElementById("computerScoreDisplay");
     let playerScore = 0;
     let computerScore = 0;
+    let gameOver = false;
 
-    // Need function playGame to accept argument of player's choice
+    
     function playGame(playerChoice){
 
+        if(gameOver) return;
+
         // Make computer choose an option each time player chooses an option
+        const choices = ["rock", "paper", "scissors"];
         const computerChoice = choices[Math.floor(Math.random() * 3)];
-        // Make result to be displayed later
+        
         let result = "";
-        // If the player's choice matches the computer's choice, make the result a tie
+        
         if(playerChoice === computerChoice){
             result = "IT'S A DRAW!!";
         }
@@ -26,51 +35,44 @@
             // Make a switch with a case for each potential player choice
             switch(playerChoice){
                 case "rock":
-                    // If the player's choice is rock but the computer's choice is paper, you win.
-                    // Otherwise you lose
                    result = (computerChoice === "scissors") ? "FATALITY!" : "WASTED...";
                    break;
+
                 case "paper":
-                    // If the player's choice is paper but the computer's choice is rock, you win.
-                    // Otherwise you lose
                    result = (computerChoice === "rock") ? "FATALITY!" : "WASTED...";
                    break;
+
                 case "scissors":
-                    // If the player's choice is scissors but the computer's choice is paper, you win.
-                    // Otherwise you lose
                    result = (computerChoice === "paper") ? "FATALITY!" : "WASTED...";
                    break;
             }
         }
-        // Make player display and computer display display respective choices
+        
         playerDisplay.textContent = `PLAYER: ${playerChoice}`;
         computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
-        // Make result display display the result
         resultDisplay.textContent = result;
 
         // Want to change text color depending on win conditions
-
         // Remove text color classes before the switch or the text will stay red on every condition
         resultDisplay.classList.remove("greenText", "redText");
 
-        // Make switch to change text color
         switch(result){
-            // If player wins, turn text green
             case "FATALITY!":
                 resultDisplay.classList.add("greenText");
-                // Increase player score when player wins
                 playerScore++;
-                // Update player score display to show player score
                 playerScoreDisplay.textContent = playerScore;
                 break;
-            // If player loses, turn text red
+            
             case "WASTED...":
                 resultDisplay.classList.add("redText");
-                // Increase computer score when computer wins
                 computerScore++;
-                // Update computer score display to show computer score
                 computerScoreDisplay.textContent = computerScore;
                 break;
+        }
+
+        if(playerScore === 5 || computerScore === 5){
+            resultDisplay.textContent = (playerScore === 5) ? "VICTORY!" : "FOILED AGAIN...";
+            gameOver = true;
         }
 
     }
